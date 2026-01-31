98d92c422a461e4af79be5d189bc6970120111b4
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2026-01-31 09:04:48 +0000

    fix archive.sh

---
+++ hpc/archive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ff-arxv
+#SBATCH --time=36:00:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=1
+#SBATCH --output=logs/archive_%j.out
+#SBATCH --error=logs/archive_%j.err
+
+# Get the current username
+USERNAME=$(whoami)
+
+LOCAL="/mnt/parscratch/users/$USERNAME/data/iBEAt_Build"
+REMOTE="login1:/shared/abdominal_imaging/Archive/iBEAt_Build"
+
+rsync -av --no-group --no-perms --delete "$LOCAL/kidney_shape" "$REMOTE"
